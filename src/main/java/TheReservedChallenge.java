e42453a70d433239c22839ab74a4141647ea5e5f
rafaeldelnero <rafael.delnero@fabfitfun.com>
2021-01-16 09:16:55 -0300

    - The reserved challenge.

---
+++ src/main/java/TheReservedChallenge.java
@@ -0,0 +1,13 @@
+public class TheReservedChallenge {
+
+  public static void main(String[] args) {
+    var permits = "Permitting";
+    var record = 777;
+    var sealed = Double.valueOf(0.0);
+    var var = "ThisIsAVar";
+    var yield = 8_000;
+
+    System.out.println(permits + record + sealed + var + yield);
+  }
+
+}
